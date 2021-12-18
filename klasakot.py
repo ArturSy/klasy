31f93ba8f30aed1c40586eb0fb42677889c01165
ArturSy <spamdoartura@gmail.com>
2021-12-18 13:59:21 +0100

    KLASYKOTA

---
+++ klasakot.py
@@ -0,0 +1,25 @@
+class kot:
+    def __init__(self):
+        self.imie = 0
+        self.kolorOczu = 0
+        self.kolorSiersci = 0
+        self.dlugosci = 0
+        self.wysokosc = 0
+        self.wiek = 0
+        self.waga = 0
+    def miauczenie(self):
+        return "miauuuu"
+    def jedzenie(self):
+        return "mniamniaminiam"
+    def spanie(self):
+        return "chrap chrap"
+    def drapanie(self):
+        return "drap drap drap"
+    def mruczenie(self):
+        return "mrrrrrrrr"
+    
+
+
+
+
+#miauczenie jedzenie spanie drapanie mruczenie
